96b279efa20936a924821ed95865d4f18ea58399
Mudit jain <muditj214@gmail.com>
2025-03-22 22:47:13 +0530

    Server Setup and Database Connected Successfully

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose'; 
+import cors from 'cors';  // enable Cross-Origin Resource Sharing
+import { nanoid } from 'nanoid';  // generate unique ids
+import dotenv from 'dotenv';  // Loads environment variables from .env
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // parse JSON bodies
+
+mongoose.connect(process.env.DATABASE_URL)
+     .then(() => console.log('Connected to MongoDB'))
+     .catch(err => console.log(err));
+
+app.listen(process.env.PORT, () => { 
+    console.log(`Server running on port ${process.env.PORT}`); 
+});
