1cf79cab2054a8d6acf6b0f9f574926f764ccdfe
Mudit jain <muditj214@gmail.com>
2025-03-22 23:05:45 +0530

    Database Schema and model created

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from 'express'; // using Es modules
 import mongoose from 'mongoose'; 
 import cors from 'cors';  // enable Cross-Origin Resource Sharing
 import { nanoid } from 'nanoid';  // generate unique ids
@@ -12,6 +12,17 @@ app.use(express.json()); // parse JSON bodies
 mongoose.connect(process.env.DATABASE_URL)
      .then(() => console.log('Connected to MongoDB'))
      .catch(err => console.log(err));
+
+const urlSchema = new mongoose.Schema({
+    originalUrl: String,
+    shortUrl: String,
+    clicks: {
+        type: Number,
+        default: 0
+    },
+});  
+
+const Url = mongoose.model('Url', urlSchema);
 
 app.listen(process.env.PORT, () => { 
     console.log(`Server running on port ${process.env.PORT}`); 
